41aa22d51812da5bdae7ffc2cdc4a85ec5cb339a
/usr/bin/vim <mike.wazovzky@gmail.com>
2017-03-17 15:41:26 +0300

    moved 2 beget

---
+++ wp-content/themes/caos-child/footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Caos
+ */
+
+?>
+<div class="clearfix"></div>
+
+<?php if ( !is_single() ) { ?>            
+</div><!-- /#container -->
+<?php } ?>
+
+	<div class="sub-footer">
+        <div class="container">
+            <div class="text-center">
+
+                    Developed by <a href="https://m-lab.xyz/">Monster Lab.</a>
+                    Powered by <a href="https://wordpress.org/"">WordPress</a>
+
+            </div>
+         </div><!-- .container -->
+    </div><!-- .sub-footer -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
