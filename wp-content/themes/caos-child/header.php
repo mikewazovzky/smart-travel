52a3e3c42b38929c76636c4618611d3cc1bdd188
Mike <mike.wazovzky@gmail.com>
2017-03-21 11:31:02 +0300

    Yandex Metrica Code

--- wp-content/themes/caos-child/header.php
+++ wp-content/themes/caos-child/header.php
@@ -21,6 +21,35 @@
 <?php wp_head(); ?>
 <!-- End WP_Head -->
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter43558969 = new Ya.Metrika({
+                    id:43558969,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/43558969" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </head>
 <?php
 //Check if Rest Api plugin is enable
